562329a68068e68c881d47a23ca303857196eacd
ATM-TheChild <98564548+ATM-TheChild@users.noreply.github.com>
2024-09-27 23:26:19 -0400

    11:25pm 27-09-2024

--- app/src/main/java/com/example/thehealthapp/Account.java
+++ app/src/main/java/com/example/thehealthapp/Account.java
@@ -1,5 +1,31 @@
+package com.example.thehealthapp;
+
+import java.util.Date;
+
 public class Account {
-    String name;
+    String fristName;
+    String lastName;
     String email;
+    String password;
+
+    Sting focusedImprovemnet;
+
+    Date wakeUp;
+    Date sleep;
+
+
+
+
+    int point;
+    int coins;
+
+
     
+    
+}
+
+class Character{
+    int[] states = new int[5];
+    //todo png
+    String png;
 }
